5909074f1607bff06377ea150289b04ed3e6382d
Ashish Rathaur <121791024+iamashishrathaur@users.noreply.github.com>
2024-07-10 21:20:21 +0530

    change to ammount section

--- src/pages/My.jsx
+++ src/pages/My.jsx
@@ -1,9 +1,23 @@
-import { FaAngleRight, FaMobileScreen, FaUnlockKeyhole } from 'react-icons/fa6'
+import { FaAngleDown, FaAngleRight, FaArrowDown } from 'react-icons/fa6'
 import BottomNavigation from '../Components/BottomNavigation'
 import VIP from '../assets/VIP.webp'
 import Avatar from '../assets/avatar.webp'
+import { useState } from 'react'
+import INR from '../assets/INR.svg'
 
 const My = () => {
+  const [isToast, setIsToast] = useState(false)
+
+  const handleCopy =()=>{
+    setIsToast(true)
+    setTimeout(()=>{
+      setIsToast(false)
+    },1000)
+ }
+
+ const handleRefresh =()=>{
+
+ }
   return (
     <>
     <div>
@@ -15,42 +29,55 @@ const My = () => {
           <div className='ms-3 text-black text-lg font-bold flex items-center'>
              <span >7905321205</span>
              {/* <span >VIP 1</span> */}
-             <i className='fa fa-copy px-3 cursor-pointer'></i>
+             <i className='fa fa-copy px-3 cursor-pointer z-30' onClick={handleCopy}></i>
+             {isToast && <div className='text-base font-normal bg-gray-400 px-3 py-0.5 rounded-3xl -ml-16 -mt-14 text-white'>Copied</div>}
           </div>
+
 
           <div className='absolute w-[150px] right-4'>
             <img src="" alt="" srcSet={VIP} />
           </div>
 
          </div>
-
        </div>
-      <div className='p-5 pb-52 -mt-20'>
-         <div className='bg-white w-full flex flex-col min-h-32 rounded-xl shadow-lg text-[#333] text-sm px-3 pt-2'>
-              {/* <div className='w-1/2 border-r'>
-                  <div className='flex items-center text-black'><strong className=''>Amount</strong> <span>203</span></div>
-                  <div className=''><button className='bg-'>Withdraw</button></div>
+      <div className='p-5 pb-52 -mt-[108px]'>
+         <div className='bg-white w-full flex flex-col min-h-52 rounded-xl shadow-lg text-[#333] text-sm px-3 pt-2'>
+              {/* <FaAngleDown/> */}
+              <div className='border rounded-[.625rem] py-[10] flex justify-between relative items-center cursor-pointer h-[39px] my-4' >
+                 <div className='px-[15px] flex items-center w-1/4'>
+                     <img width={100} src={INR} alt="" className='w-4 h-4 mr-2.5'/>
+                     <span className='mr-[18px] ml-auto text-black'>100</span>         
+                 </div>
+                  <div className='w-2/3 flex justify-end'>
+                    <i className="fa-solid fa-angle-down"></i>
+                  </div>
+
+                  <div className='w-1/6 h-full pl-3'>
+                      <div className='p-[10px_15px] flex justify-center h-full bg-[#b7d0ff] rounded-tr-[.625rem] rounded-br-[.625rem]' onClick={handleRefresh}>
+                        <div className='flex items-center cursor-pointer'>
+                          <i className="fa fa-arrows-rotate text-lg"></i>
+                        </div>
+                      </div>
+                  </div>
               </div>
-              <div className='w-1/2l'>
-              <div><button>Recharge</button></div>
-              </div> */}
+          
                 <span className='text-base text-black'>Frequently used</span>
                 <div className='flex pt-5 text-xs font-medium'>
                     <div className='w-1/4 flex flex-col items-center'>
                     {/* <i className="fa-solid fa-wallet"></i> */}
-                       <div className='bg-[#ff7cbe] h-10 w-10 rounded-lg z-20 shadow-sm flex items-center justify-center'><i className="fa fa-piggy-bank text-white text-xl"></i></div>
+                       <div className='bg-[#ff7cbe] h-10 w-10 rounded-lg z-20 shadow-sm flex items-center justify-center cursor-pointer'><i className="fa fa-piggy-bank text-white text-xl"></i></div>
                        <div className='mt-1'>Recharge</div>
                     </div>
                     <div className='w-1/4 flex flex-col items-center'>
-                    <div className='bg-[#60cfff] h-10 w-10 rounded-lg z-20 shadow-sm flex items-center justify-center'> <i className="fa-sharp fa-solid fa-building-columns text-white text-xl"></i></div>
+                    <div className='bg-[#60cfff] h-10 w-10 rounded-lg z-20 shadow-sm flex items-center justify-center cursor-pointer'> <i className="fa-sharp fa-solid fa-building-columns text-white text-xl"></i></div>
                       <div className='mt-1'>Withdraw</div>
                     </div>
                     <div className='w-1/4 flex flex-col items-center'>
-                    <div className='bg-[#fdb953] h-10 w-10 rounded-lg z-20 shadow-sm flex items-center justify-center'> <i className="fa-solid fa-money-bill-trend-up text-white text-xl"></i></div>
+                    <div className='bg-[#fdb953] h-10 w-10 rounded-lg z-20 shadow-sm flex items-center justify-center cursor-pointer'> <i className="fa-solid fa-money-bill-trend-up text-white text-xl"></i></div>
                     <div className='mt-1'>Transaction</div>
                     </div>
                     <div className='w-1/4 flex flex-col items-center'>
-                    <div className='bg-[#5dffe9] h-10 w-10 rounded-lg z-20 shadow-sm flex items-center justify-center'><i className="fa-solid fa-people-group text-white text-xl"></i></div>
+                    <div className='bg-[#42e1cc] h-10 w-10 rounded-lg z-20 shadow-sm flex items-center justify-center cursor-pointer'><i className="fa-solid fa-people-group text-white text-xl"></i></div>
                     <div className='mt-1'>Team</div>
                     </div>
                 </div>
