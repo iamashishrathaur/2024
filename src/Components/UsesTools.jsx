2810ada06aabe35321178acd80b5f28dee8d7d6d
Ashish Rathaur <121791024+iamashishrathaur@users.noreply.github.com>
2024-07-14 08:31:05 +0530

    remove scrollbar if open mobile

--- src/Components/UsesTools.jsx
+++ src/Components/UsesTools.jsx
@@ -4,17 +4,17 @@ const UsesTools = () => {
   return (
     <div className='w-full'>
         <div className='text-black font-medium text-xl font-[Poppins] p-1 pt-7 pb-3 '>More Useful Tools</div>
-         <div className='flex w-full -mx-3'>
-            <div className='w-1/2 p-3 min-h-20'> 
-              <div className='flex h-full bg-[#DFF7FF] shadow-sm rounded-2xl p-3 items-center '>
+         <div className='flex w-full'>
+            <div className='w-1/2 pr-3 min-h-14 flex'> 
+              <div className='flex h-full w-full bg-[#DFF7FF] shadow-sm rounded-2xl p-3 items-center '>
                 <div className='h-[35px] w-[35px] rounded-full bg-[#3F81FD] flex justify-center items-center'>
                     <i className='fa fa-headset text-white'></i>
                 </div>
                 <span className='italic text-black font-semibold px-3'>SUPPORT</span>
               </div>
             </div>
-            <div className=' w-1/2 p-3 min-h-20'>
-              <div className='flex h-full bg-[#FFDFDF] shadow-sm rounded-2xl p-3 items-center'>
+            <div className=' w-1/2 pl-3 min-h-14 flex'>
+              <div className='flex h-full w-full bg-[#FFDFDF] shadow-sm rounded-2xl p-3 items-center'>
                 <div className='h-[35px] w-[35px] rounded-full bg-[#F9676E] flex justify-center items-center'>
                 <i className="fa-solid fa-gift text-white" />    
                 </div>
