56d98418433e3a775c82ad1ed84e7e95a5183d41
Ashish Rathaur <121791024+iamashishrathaur@users.noreply.github.com>
2024-07-14 12:12:13 +0530

    strike update

--- src/Components/WeaklyIncome.jsx
+++ src/Components/WeaklyIncome.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Avatar from '../assets/avatar.webp'
 
 const WeaklyIncome = () => {
-  const [percentage, setPercentage] = useState(70);
+  const [percentage, setPercentage] = useState(10);
 
   const radius = 30;
   const circumference = 2 * Math.PI * radius;
@@ -13,24 +13,15 @@ const WeaklyIncome = () => {
     <div className="flex flex-col ">
     <div className="text-lg -mt-1 mb-3">Total Earnings</div>
     <div className="text-3xl font-extrabold">₹ 8,120.09</div>
- 
+
 
     <div className="absolute flex items-center justify-center bottom-2">
-    <svg className="transform rotate-90" width="80" height="80" viewBox="0 0 80 80">
-          <circle cx="40" cy="40" r={radius} stroke="rgba(255, 255, 255, 0.5)" strokeWidth="6" fill="none" />
-          <circle cx="40" cy="40" r={radius} stroke="white" strokeWidth="6" fill="none" strokeDasharray={circumference} strokeDashoffset={strokeDashoffset} className="transition-stroke-dashoffset duration-500" />
+    <svg className="transform -rotate-90 rounded-full"  width="80" height="80" viewBox="0 0 80 80">
+    <circle cx="40" cy="40" r={radius} stroke="rgba(255, 255, 255, 0.5)" strokeWidth="6" fill="none" />
+    <circle cx="40" cy="40" r={radius} stroke="white" strokeWidth="6" fill="none" strokeDasharray={circumference} strokeDashoffset={strokeDashoffset} className="transition-stroke-dashoffset duration-500" strokeLinecap="round" />
         </svg>
         <i className="fa fa-fire absolute text-white"></i>
       </div>
-    {/* <div className="flex items-end h-[100px]">
-            <div className="h-24 w-2 bg-white bg-opacity-50 gap-1 rounded-t-full rounded-b-full mr-8 relative bar after:h-[10%]"></div>
-            <div className="h-24 w-2 bg-white bg-opacity-50 gap-1 rounded-t-full rounded-b-full mr-8 relative bar after:h-[30%]"></div>
-            <div className="h-24 w-2 bg-white bg-opacity-50 gap-1 rounded-t-full rounded-b-full mr-8 relative bar after:h-[70%]"></div>
-            <div className="h-24 w-2 bg-white bg-opacity-50 gap-1 rounded-t-full rounded-b-full mr-8 relative bar after:h-[60%]"></div>
-            <div className="h-24 w-2 bg-white bg-opacity-50 gap-1 rounded-t-full rounded-b-full mr-8 relative bar after:h-[100%]"></div>
-            <div className="h-24 w-2 bg-white bg-opacity-50 gap-1 rounded-t-full rounded-b-full mr-8 relative bar after:h-[20%]"></div>
-            <div className="h-24 w-2 bg-white bg-opacity-50 gap-1 rounded-t-full rounded-b-full mr-8 relative bar after:h-[40%]"></div>
-    </div> */}
   </div>
   <div className="flex flex-col items-center h-full w-auto bg-white p-4 rounded-2xl relative">
     <img src={Avatar} width={100} className='w-14 h-14 rounded-full mb-[10px] bg-slate-300'/>
